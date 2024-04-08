396a5ec140afd3c822a34fdcdb2b1c721dc9dd46
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-04-08 18:48:14 -0300

    [STYLE] Home page structure + [FEAT] Adding variables

---
+++ config/vars.php
@@ -0,0 +1,19 @@
+<?php
+$app_title = "Binance Trade Bot";
+$app_version = "1.0.0";
+$app_testnet = 1; // 1 = testnet, 2 = mainnet
+
+// Base endpoints
+$app_endpoint_api = "https://api.binance.com/api";
+$app_endpoint_ws_api = "wss://ws-api.binance.com/ws-api/v3";
+$app_endpoint_ws = "wss://stream.binance.com:9443/ws";
+$app_endpoint_stream = "wss://stream.binance.com:9443/stream";
+
+if($app_network === 1){
+    $app_endpoint_api = "https://testnet.binance.vision/api";
+    $app_endpoint_ws_api = "wss://testnet.binance.vision/ws-api/v3";
+    $app_endpoint_ws = "wss://testnet.binance.vision/ws";
+    $app_endpoint_stream = "wss://testnet.binance.vision/stream";
+}
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'src/database.php';
+require_once 'config/vars.php';
 // Estabelece a conexão com o banco de dados
 $conn = connectDatabase();
 ?>
@@ -18,7 +19,44 @@ $conn = connectDatabase();
     <!-- JQuery 3.7.1 -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 </head>
-<body>
-    
+<body class="bg-black text-white">
+    <div class="container">
+        <div class="row bg-dark rounded-bottom-2">
+            <div class="col-md-12 text-center p-1">
+                <h3 class="display-5 p-3 text-uppercase fw-bold">
+                    <?php echo $app_title; ?>
+                </h3>
+                <span class="float-start">Developer: Francis Santiago</span>
+                <span class="float-end">Version: <?php echo $app_version; ?></span>
+            </div>
+        </div>
+        <div class="row bg-light text-dark d-flex align-items-center">
+            <div class="col-md-5">
+                <div class="input-group mb-3 mt-3">
+                    <span class="input-group-text">@</span>
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="binance_api_key" placeholder="Binance API Key">
+                        <label for="binance_api_key">Binance API Key</label>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="input-group mb-3 mt-3 input-group">
+                    <span class="input-group-text">@</span>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="binance_api_secret" placeholder="Binance API Secret">
+                        <label for="binance_api_secret">Binance API Secret</label>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="input-group mb-3 mt-3">
+                    <button type="submit" class="btn btn-success form-control btn-md fw-bold">CONNECT</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </body>
 </html>
